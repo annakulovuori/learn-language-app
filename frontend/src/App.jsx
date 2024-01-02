7979b9031d2d3bf44a8c285ecbb27cc754b70ab5
Anna Kuovuori <anna.kulovuori@tuni.fi>
2024-01-02 12:54:04 +0200

    ADD vite frontend, React Router and react files Admin, Home, Info, Navbar and Player

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Player from "./Player";
+import Admin from "./Admin";
+import Info from "./Info";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/player" element={<Player />}></Route>
+        <Route path="/admin" element={<Admin />}></Route>
+        <Route path="/info" element={<Info />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,15 @@
+//import "./navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/play">play</a>
+        <a href="/add">add</a>
+        <a href="/info">info</a>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
