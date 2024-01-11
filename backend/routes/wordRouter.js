5c2ff48e8bb046dc2391d4fb07c857621e4381a0
Anna Kuovuori <anna.kulovuori@tuni.fi>
2024-01-11 12:31:36 +0200

    EDIT get findAll to wordRouter.js

--- backend/routes/wordRouter.js
+++ backend/routes/wordRouter.js
@@ -2,6 +2,14 @@ const database = require("./database/database");
 const express = require("express");
 const wordRouter = express.Router();
 
-
+wordRouter.get("/", async (req, res) => {
+  try {
+    const words = await database.findAll();
+    res.json(words);
+  } catch (err) {
+    res.status(500).json({ msq: err });
+  }
+});
 
 module.exports = wordRouter;
+
