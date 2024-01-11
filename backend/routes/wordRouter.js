45eac0dfa0cb92cf0e1ecc4b901eec10226b077d
Anna Kuovuori <anna.kulovuori@tuni.fi>
2024-01-11 14:40:50 +0200

    EDIT post route to wordRouter.js

--- backend/routes/wordRouter.js
+++ backend/routes/wordRouter.js
@@ -1,4 +1,4 @@
-const database = require("./database/database");
+const database = require("../database/database");
 const express = require("express");
 const wordRouter = express.Router();
 
@@ -7,9 +7,19 @@ wordRouter.get("/", async (req, res) => {
     const words = await database.findAll();
     res.json(words);
   } catch (err) {
-    res.status(500).json({ msq: err });
+    res.status(500).json({ msg: err });
+  }
+});
+
+wordRouter.post("/", async (req, res) => {
+  try {
+    const word1 = req.body.word1;
+    const word2 = req.body.word2;
+    const response = await database.saveWord(word1, word2);
+    res.status(201).json(response);
+  } catch (err) {
+    res.status(500).json({ msg: err });
   }
 });
 
 module.exports = wordRouter;
-
