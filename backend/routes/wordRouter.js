3e24f0a687a55bc0ff3fbc161efc93cb612de147
Anna Kuovuori <anna.kulovuori@tuni.fi>
2024-01-16 13:31:48 +0200

    EDIT delete and put routes to wordRouter.js

--- backend/routes/wordRouter.js
+++ backend/routes/wordRouter.js
@@ -22,4 +22,38 @@ wordRouter.post("/", async (req, res) => {
   }
 });
 
+wordRouter.delete("/:myId([0-9]+)", async (req, res) => {
+  try {
+    const id = parseInt(req.params.myId);
+    const result = await database.deleteById(id);
+    res.status(204).json(result);
+  } catch (err) {
+    res.status(400).json({ msg: "cannot find ID" });
+  }
+});
+
+wordRouter.put("/:myId([0-9]+)", async (req, res) => {
+  try {
+    const expectedKeys = ["word1", "word2"];
+    const receivedKeys = Object.keys(req.body);
+
+    if (
+      expectedKeys[0] !== receivedKeys[0] ||
+      expectedKeys[1] !== receivedKeys[1]
+    ) {
+      return res.status(400).json({
+        error: "Invalid keys.",
+      });
+    }
+
+    const id = parseInt(req.params.myId);
+    const updatedWords = req.body;
+
+    const result = await database.updateById(id, updatedWords);
+    res.json(result);
+  } catch (err) {
+    res.status(404).send({ msg: "cannot find ID" });
+  }
+});
+
 module.exports = wordRouter;
