d76c0ddd91179f61293ae4778c16875f09246aa5
Anna Kuovuori <anna.kulovuori@tuni.fi>
2024-01-17 20:06:25 +0200

    DELETE all EDIT doing it again using material.ui

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,15 +1,32 @@
-import "./navbar.css";
+//import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import { Link } from "react-router-dom";
+import Container from "@mui/material/Container";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="links">
-        <a href="/">Home</a>
-        <a href="/player">player</a>
-        <a href="/admin">admin</a>
-        <a href="/info">info</a>
-      </div>
-    </nav>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: {
+                xs: "none",
+                md: "flex",
+                gap: "3rem",
+              },
+            }}
+          >
+            <Link to={"/home"}>Home</Link>
+            <Link to={"/admin"}>Admin</Link>
+            <Link to={"/player"}>Player</Link>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 export default Navbar;
