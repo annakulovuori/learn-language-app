1af39c5b80c5c4680401ec228b4f38122d3225aa
Anna Kuovuori <anna.kulovuori@tuni.fi>
2024-01-17 20:08:22 +0200

    EDIT using material.ui shaping and creating the form, fetchData function and useEffect

--- frontend/src/Player.jsx
+++ frontend/src/Player.jsx
@@ -1,10 +1,56 @@
+import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Typography } from "@mui/material";
+import axios from "axios";
 
-import "./player.css"
+export default function Player() {
+  const [words, setWords] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/words");
+      setWords(response.data);
+    } catch (err) {
+      console.error("Error fetching data:", err);
+    }
+  };
 
-const Player = () => {
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-};
-
-
-export default Player;
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "25ch" },
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      {words.map((wordObj, index) => (
+        <div
+          key={index}
+          style={{
+            display: "flex",
+            alignItems: "baseline",
+            justifyContent: "center",
+            gap: "2 0px",
+          }}
+        >
+          <Typography variant="h6">{wordObj.word1}</Typography>
+          <TextField
+            id={`standard-basic-${index}`}
+            label={wordObj.word2}
+            variant="standard"
+          />
+        </div>
+      ))}
+    </Box>
+  );
+}
