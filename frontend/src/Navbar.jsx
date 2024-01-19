021d2ab06ea1065701cb1f37cf8b512cdf3ace99
Anna Kuovuori <anna.kulovuori@tuni.fi>
2024-01-19 11:51:45 +0200

    EDIT navbar styling with material.ui, all buttons spread evenly taking whole navbar space, pink details

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,32 +1,54 @@
-//import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
 
 function Navbar() {
+  const location = useLocation();
+
+  const isActive = (path) => location.pathname === path;
+
   return (
-    <AppBar position="static">
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: "#EAEAEA",
+      }}
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box
             sx={{
-              flexGrow: 1,
-              display: {
-                xs: "none",
-                md: "flex",
-                gap: "3rem",
-              },
+              display: "flex",
+              justifyContent: "space-between", // Hajauttaa napit tasaisesti
+              flexGrow: 1, // Hycäksyy että napit voi täyttää jäljelle jääneen tilan
             }}
           >
-            <Link to={"/home"}>Home</Link>
-            <Link to={"/admin"}>Admin</Link>
-            <Link to={"/player"}>Player</Link>
+            {["/home", "/admin", "/player", "/info"].map((path, index) => (
+              <Button
+                key={index}
+                component={Link}
+                to={path}
+                variant="text"
+                sx={{
+                  flex: 1, // Jokainen nappi vie saman verran tilaa
+                  textTransform: "none",
+                  fontSize: "1.2rem",
+                  color: "#333333",
+                  "&:hover": { backgroundColor: "#EFB4FA" },
+                  ...(isActive(path) && { borderBottom: "3px solid #EFB4FA" }),
+                }}
+              >
+                {path.substring(1)}
+              </Button>
+            ))}
           </Box>
         </Toolbar>
       </Container>
     </AppBar>
   );
 }
+
 export default Navbar;
