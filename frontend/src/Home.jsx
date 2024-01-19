fc8293448ec079acc8d585e409e33f8f7933ffe2
Anna Kuovuori <anna.kulovuori@tuni.fi>
2024-01-19 14:00:43 +0200

    EDIT homepage text, styling title by making div around it

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,3 +1,37 @@
-function Home() {}
+function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "25px",
+        fontFamily: "Roboto, sans-serif",
+        color: "#292929",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#EFB4FA",
+          width: "45%",
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
+        <p style={{ fontSize: "40px" }}>READY TO LEARN?</p>
+      </div>
+      <p style={{ fontSize: "20px" }}>
+        ON THE PLAYER TAB YOU CAN PRACTICE WORDS
+      </p>
+      <p style={{ fontSize: "20px" }}>
+        ON THE ADMIN TAB YOU CAN CREATE AND EDIT WORD PAIRS
+      </p>
+      <p style={{ fontSize: "20px" }}>
+        TO FIND MORE INFO PLEASE CLICK YOURSELF INTO THE INFO TAB
+      </p>
+    </div>
+  );
+}
 
 export default Home;
